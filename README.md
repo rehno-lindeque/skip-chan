6903d2df58e99abb3081691702002bfef9ab1680
Rehno Lindeque <errantkid@gmail.com>
2019-08-26 23:14:49 -0400

    rename and inline pragmas

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # skip-chan
 
-Please note that this is not my own original work, it's merely a packaged (but not maintained or published) version of the skip channel example in the [Concurrent Haskell](https://www.microsoft.com/en-us/research/wp-content/uploads/1996/01/concurrent-haskell.pdf) paper.
+This is a modified version of the skip channel example in the [Concurrent Haskell](https://www.microsoft.com/en-us/research/wp-content/uploads/1996/01/concurrent-haskell.pdf) paper.
+
+Note that it is not published, nor actively documented or maintained for the time being.
--- src/Control/Concurrent/SkipChan.hs
+++ src/Control/Concurrent/SkipChan.hs
@@ -4,29 +4,33 @@ import Control.Concurrent.MVar
 
 data SkipChan a = SkipChan (MVar (a, [MVar ()])) (MVar ())
 
+{-# inline newSkipChan #-}
 newSkipChan :: IO (SkipChan a)
 newSkipChan = do
   sem <- newEmptyMVar
   main <- newMVar (undefined, [sem])
   return (SkipChan main sem)
 
-putSkipChan :: SkipChan a -> a -> IO ()
-putSkipChan (SkipChan main _) v = do
+{-# inline writeSkipChan #-}
+writeSkipChan :: SkipChan a -> a -> IO ()
+writeSkipChan (SkipChan main _) a = do
   (_, sems) <- takeMVar main
-  putMVar main (v, [])
+  putMVar main (a, [])
   mapM_ (\sem -> putMVar sem ()) sems
 
-getSkipChan :: SkipChan a -> IO a
-getSkipChan (SkipChan main sem) = do
+{-# inline readSkipChan #-}
+readSkipChan :: SkipChan a -> IO a
+readSkipChan (SkipChan main sem) = do
   takeMVar sem
-  (v, sems) <- takeMVar main
-  putMVar main (v, sem:sems)
-  return v
+  (a, sems) <- takeMVar main
+  putMVar main (a, sem:sems)
+  return a
 
+{-# inline dupSkipChan #-}
 dupSkipChan :: SkipChan a -> IO (SkipChan a)
 dupSkipChan (SkipChan main _) = do
   sem <- newEmptyMVar
-  (v, sems) <- takeMVar main
-  putMVar main (v, sem:sems)
+  (a, sems) <- takeMVar main
+  putMVar main (a, sem:sems)
   return (SkipChan main sem)
 
