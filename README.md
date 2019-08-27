8c4b54ca775c4790ce5b10bf9f1aeb8999f49a10
Rehno Lindeque <errantkid@gmail.com>
2019-08-26 23:17:16 -0400

    Add extend function for atomically generating new SkipChan elements from the current head

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # skip-chan
 
 This is a modified version of the skip channel example in the [Concurrent Haskell](https://www.microsoft.com/en-us/research/wp-content/uploads/1996/01/concurrent-haskell.pdf) paper.
+It adds `extendSkipChan`, `extendSkipChan_`, `extendSkipChanMasked`, `extendSkipChanMasked_`.
 
 Note that it is not published, nor actively documented or maintained for the time being.
--- src/Control/Concurrent/SkipChan.hs
+++ src/Control/Concurrent/SkipChan.hs
@@ -1,14 +1,15 @@
 module Control.Concurrent.SkipChan where
 
 import Control.Concurrent.MVar
+import Control.Exception.Base
 
 data SkipChan a = SkipChan (MVar (a, [MVar ()])) (MVar ())
 
 {-# inline newSkipChan #-}
-newSkipChan :: IO (SkipChan a)
-newSkipChan = do
-  sem <- newEmptyMVar
-  main <- newMVar (undefined, [sem])
+newSkipChan :: a -> IO (SkipChan a)
+newSkipChan a = do
+  sem <- newMVar ()
+  main <- newMVar (a, [])
   return (SkipChan main sem)
 
 {-# inline writeSkipChan #-}
@@ -29,8 +30,43 @@ readSkipChan (SkipChan main sem) = do
 {-# inline dupSkipChan #-}
 dupSkipChan :: SkipChan a -> IO (SkipChan a)
 dupSkipChan (SkipChan main _) = do
-  sem <- newEmptyMVar
-  (a, sems) <- takeMVar main
-  putMVar main (a, sem:sems)
+  sem <- newMVar ()
   return (SkipChan main sem)
 
+{-# inline extendSkipChan_ #-}
+extendSkipChan_ :: SkipChan a -> (a -> IO a) -> IO ()
+extendSkipChan_ (SkipChan main _) io =
+  mask $ \restore -> do
+    (a, sems) <- takeMVar main
+    a' <- restore (io a) `onException` putMVar main (a,sems)
+    putMVar main (a',[])
+    mapM_ (\sem -> putMVar sem ()) sems
+
+{-# inline extendSkipChan #-}
+extendSkipChan :: SkipChan a -> (a -> IO (a, b)) -> IO b
+extendSkipChan (SkipChan main _) io = do
+  mask $ \restore -> do
+    (a, sems) <- takeMVar main
+    (a', b) <- restore (io a >>= evaluate) `onException` putMVar main (a,sems)
+    putMVar main (a',[])
+    mapM_ (\sem -> putMVar sem ()) sems
+    return b
+
+{-# inline extendSkipChanMasked_ #-}
+extendSkipChanMasked_ :: SkipChan a -> (a -> IO a) -> IO ()
+extendSkipChanMasked_ (SkipChan main _) io =
+  mask_ $ do
+    (a, sems) <- takeMVar main
+    a' <- io a `onException` putMVar main (a,sems)
+    putMVar main (a',[])
+    mapM_ (\sem -> putMVar sem ()) sems
+
+{-# inline extendSkipChanMasked #-}
+extendSkipChanMasked :: SkipChan a -> (a -> IO (a, b)) -> IO b
+extendSkipChanMasked (SkipChan main _) io = do
+  mask_ $ do
+    (a, sems) <- takeMVar main
+    (a', b) <- (io a >>= evaluate) `onException` putMVar main (a,sems)
+    putMVar main (a',[])
+    mapM_ (\sem -> putMVar sem ()) sems
+    return b
